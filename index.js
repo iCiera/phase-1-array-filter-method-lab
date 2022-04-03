e0b2fc2e72ecf0e7d49cbdf52b776b76e503855c
ciera <cieranicole.m15@gmail.com>
2022-04-03 10:24:55 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+
+function findMatching(drivers, string) {
+    return drivers.filter(function(name) {
+      return name.toLowerCase().indexOf(string.toLowerCase()) !== -1
+
+    })
+}
+
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function(name) {
+     return name.toLowerCase().indexOf(string.toLowerCase()) === 0
+   })
+ }
+
+
+ function matchName(drivers,string){
+    return drivers.filter(function(element) {
+        return element.name === string
+      })
+ }
+
